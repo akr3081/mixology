8f9d28eb25722fe23849745fd2eceba60fbc0c9a
Adam Rock <Adamr34@gmail.com>
2022-04-16 18:04:38 -0400

    Renamed fragments and added button/action to move between them

--- app/src/main/java/com/mixology/RecipeListFragment.kt
+++ app/src/main/java/com/mixology/RecipeListFragment.kt
@@ -5,24 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mixology.databinding.FragmentFirstBinding
+import androidx.navigation.fragment.findNavController
+import com.mixology.databinding.RecipeListFragmentBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
-    private var _binding: FragmentFirstBinding? = null
+class RecipeListFragment : Fragment() {
+    private var _binding: RecipeListFragmentBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = RecipeListFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.recipeDetailButton.setOnClickListener {
+            findNavController().navigate(R.id.action_navigate_to_RecipeDetailFragment)
+        }
     }
 
 
