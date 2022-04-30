aa3a31ba20182aba3b295efb383bdff14df72846
Adam Rock <Adamr34@gmail.com>
2022-04-29 20:35:37 -0400

    Sets up initial navigation drawer

--- app/src/main/java/com/mixology/MainActivity.kt
+++ app/src/main/java/com/mixology/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.mixology
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.*
+import com.google.android.material.navigation.NavigationView
 import com.mixology.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,15 +20,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
         setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
         // CLicking on toolbar will navigate back to recipe list fragment
-        binding.toolbar.setNavigationOnClickListener {
+        binding.appBarMain.toolbar.setNavigationOnClickListener {
             navController.navigateUp()
         }
     }
+
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        menuInflater.inflate(R.menu.main, menu)
+//        return true
+//    }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        val navController = findNavController(R.id.nav_host_fragment_content_main)
+//        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+//    }
 }
